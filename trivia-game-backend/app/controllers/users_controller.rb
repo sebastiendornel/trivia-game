f7a035b019c53d1ac7dd63d4335c6729af5d5321
dsuccar <danielsuccar@icloud.com>
2020-04-30 16:24:54 -0400

    got Q&A's to show on front end

--- trivia-game-backend/app/controllers/users_controller.rb
+++ trivia-game-backend/app/controllers/users_controller.rb
@@ -1,8 +1,23 @@
 class UsersController < ApplicationController
     def index
         users = User.all
-        render json: users: include: [:name]
+        # render json: users: include: [:name]
+        render json: User.all.to_json(:include => :user_questions) #may not need the include clause
     end
-end
+ends
 
 
+
+
+# class TrainersController < ApplicationController
+
+#     def index
+#         render json: Trainer.all.to_json(:include => :pokemons)
+#     end
+
+#     def show
+#         trainer = Trainer.find(params[:id])
+
+#         render json: trainer.to_json(:include => :pokemons)
+#     end
+# end
